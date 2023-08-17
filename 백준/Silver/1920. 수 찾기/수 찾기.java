775bbf0c66d368c95b03023a6f530e05a3bd5478
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-08-17 23:21:46 +0900

    [Silver IV] Title: 수 찾기, Time: 628 ms, Memory: 42072 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,20 +1,42 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
 
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Map<Integer, Integer> map = new HashMap<>();
+        st = new StringTokenizer(br.readLine());
 
-        for(int i = 0; i < n; i++) map.put(sc.nextInt(), 0);
+        for (int n = 0; n < N; n++) arr[n] = Integer.parseInt(st.nextToken());
 
-        int m = sc.nextInt();
+        Arrays.sort(arr);
 
-        for(int i = 0; i < m; i++){
-            if(map.containsKey(sc.nextInt())) System.out.println("1");
-            else System.out.println("0");
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        loop: for (int m = 0; m < M; m++) {
+            int find = Integer.parseInt(st.nextToken());
+            int l = 0;
+            int r = N - 1;
+
+            while (l <= r) {
+                int mid = (l + r) / 2;
+
+                if (find == arr[mid]) {
+                    bw.write("1\n");
+                    continue loop;
+                }
+                else if (arr[mid] < find) l = mid + 1;
+                else if (find < arr[mid]) r = mid - 1;
+            }
+
+            bw.write("0\n");
         }
+        
+        bw.close();
     }
 }
