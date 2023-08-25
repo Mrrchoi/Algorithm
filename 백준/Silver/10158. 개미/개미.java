2269efc6807c5b45c9160bf3bb030268dacd5b91
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-08-26 00:05:42 +0900

    [Silver IV] Title: 개미, Time: 80 ms, Memory: 11624 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.java
+++ 백준/Silver/10158. 개미/개미.java
@@ -1,15 +1,16 @@
+import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String args[]) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String s = br.readLine();
-        int w = Integer.parseInt(s.split(" ")[0]);
-        int h = Integer.parseInt(s.split(" ")[1]);
-        s = br.readLine();
-        int p = Integer.parseInt(s.split(" ")[0]);
-        int q = Integer.parseInt(s.split(" ")[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int p = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
         int t = Integer.parseInt(br.readLine());
 
         p = ((p + t) / w) % 2 == 0 ? (p + t) % w : w - (p + t) % w;
