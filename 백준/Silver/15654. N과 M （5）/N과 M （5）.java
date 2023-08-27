91ac9e74c3815fa54c8bb7816e6aa0801ea35b3f
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-08-27 09:44:48 +0900

    [Silver III] Title: N과 M (5), Time: 2492 ms, Memory: 70352 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    public static void per(int[] source, int[] target, boolean[] visit, int tgtIdx){
+        if(tgtIdx == target.length){
+            for(int value : target) System.out.print(value + " ");
+            System.out.println();
+            return;
+        }
+
+        for(int i = 0; i < source.length; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                target[tgtIdx] = source[i];
+                per(source, target, visit, tgtIdx + 1);
+                visit[i] = false;
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] source = new int[n];
+        boolean[] visit = new boolean[n];
+        int[] target = new int[m];
+
+        for(int i = 0; i < n; i++) source[i] = sc.nextInt();
+
+        Arrays.sort(source);
+
+        per(source, target, visit, 0);
+    }
+}
