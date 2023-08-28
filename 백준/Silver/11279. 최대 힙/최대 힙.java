84fcfec1fa57b551e39c2fbf00bf9502f3af4e24
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-08-28 09:51:08 +0900

    [Silver II] Title: 최대 힙, Time: 304 ms, Memory: 27144 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            int k = Integer.parseInt(br.readLine());
+
+            if(k != 0) pq.offer(k);
+            else{
+                if(pq.size() == 0) bw.write("0\n");
+                else bw.write(pq.poll() + "\n");
+            }
+
+        }
+
+        bw.close();
+    }
+}
