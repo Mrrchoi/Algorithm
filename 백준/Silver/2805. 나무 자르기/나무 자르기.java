f411e95412573f4e050a2bcb361da24efd76da59
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-08-30 11:08:16 +0900

    [Silver II] Title: 나무 자르기, Time: 1680 ms, Memory: 308592 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        int left = 1, right = 0;
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+            if(arr[i] > right) right = arr[i];
+        }
+
+        while(left <= right){
+            int mid = (left + right) / 2;
+            long sum = 0;
+
+            for(int i = 0; i < n; i++) sum += arr[i] > mid ? arr[i] - mid : 0;
+
+            if(sum >= m) left = mid + 1;
+            else if(sum < m) right = mid - 1;
+        }
+
+        System.out.println(left - 1);
+    }
+}
