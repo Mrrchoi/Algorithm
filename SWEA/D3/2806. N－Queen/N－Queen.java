700968d60227f9139f76063af76c181dc05e17e0
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-09-01 16:10:08 +0900

    [D3] Title: N-Queen, Time: 119 ms, Memory: 19,472 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -1,34 +1,30 @@
 import java.io.*;
-import java.util.*;
-import java.awt.Point;
 
 public class Solution {
-    public static int[][] chess;
-    public static int[][] delta = {{-1, -1}, {-1, 0}, {-1, 1}};
+    public static int[] chess;
     public static int count;
     public static int n;
-    public static void n_queen(int r) {
-        if(r == n){
+    public static void n_queen(int idx) {
+        if(idx == n){
             count++;
             return;
         }
 
-        loop: for(int i = 0; i < n; i++){
-            for(int j = 0; j < 3; j++){
-                for(int k = 1;; k++){
-                    int next_r = r + delta[j][0] * k;
-                    int next_c = i + delta[j][1] * k;
-                    
-                    if(next_r < 0 || next_r >= n || next_c < 0 || next_c >= n) break;
-                    else if(chess[next_r][next_c] == 1) continue loop;
-                }
+        for(int i = 0; i < n; i++){
+            chess[idx] = i;
+            if(promising(idx)) {
+                n_queen(idx + 1);
             }
-            
-            chess[r][i] = 1;
-            n_queen(r + 1);
-            chess[r][i] = 0;
         }
     }
+    public static boolean promising(int idx){
+        for(int i = 0; i < idx; i++){
+            if(chess[i] == chess[idx] || Math.abs(i - idx) == Math.abs(chess[i] - chess[idx])) return false;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -36,7 +32,7 @@ public class Solution {
 
         for(int t = 1; t <= test_case; t++){
             n = Integer.parseInt(br.readLine());
-            chess = new int[n][n];
+            chess = new int[n];
             count = 0;
 
             n_queen(0);
