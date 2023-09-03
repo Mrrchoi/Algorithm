90d36ebe0a6ef4bbf437ae34411079cc425ad4d0
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-09-03 11:31:17 +0900

    [Silver II] Title: N과 M (12), Time: 400 ms, Memory: 18080 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void com(int[] source, int[] target, int tgtIdx, int srcIdx){
+        if(tgtIdx == target.length){
+            for(int value : target) System.out.print(value + " ");
+            System.out.println();
+            return;
+        }
+
+        for(int i = srcIdx; i < source.length; i++){
+            if(i != 0 && source[i] == source[i - 1]) continue;
+            target[tgtIdx] = source[i];
+
+            com(source, target, tgtIdx + 1, i);
+        }
+
+    }
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] source = new int[n];
+        int[] target = new int[m];
+
+        for(int i = 0; i < n; i++) source[i] = sc.nextInt();
+
+        Arrays.sort(source);
+
+        com(source, target, 0, 0);
+    }
+}
