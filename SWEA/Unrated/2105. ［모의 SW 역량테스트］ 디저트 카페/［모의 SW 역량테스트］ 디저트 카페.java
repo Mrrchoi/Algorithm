c26697567a07ebdd2e60474f47751212475847b9
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-09-04 12:44:03 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 281 ms, Memory: 95,252 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
@@ -7,7 +7,6 @@ public class Solution {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         int test_case = Integer.parseInt(br.readLine());
-        int[][] delta = {{1, -1}, {1, 1}}; //좌하향, 우하향
 
         for(int t = 1; t <= test_case; t++) {
             int n = Integer.parseInt(br.readLine());
@@ -22,45 +21,41 @@ public class Solution {
 
             for(int i = 0; i < n - 2; i++){
                 for(int j = 1; j < n - 1; j++){
-                    //우하향쪽으로 갔을 때 유효한 해를 가질 수 있는 범위로 제한
-                    for(int k = 1;; k++){ // 좌측 꼭짓점 정하기
-                        int left_point_r = i + delta[0][0] * k;
-                        int left_point_c = j + delta[0][1] * k;
+                    for(int k = 1;; k++){
+                        int left_point_r = i + k;
+                        int left_point_c = j + - k;
 
                         if(left_point_r >= n - 1 || left_point_c < 0) break;
 
-                        loop: for(int l = 1;; l++){ //우측 꼭짓점 정하기
-                            int right_point_r = i + delta[1][0] * l;
-                            int right_point_c = j + delta[1][1] * l;
+                        loop: for(int l = 1;; l++){
+                            int right_point_r = i + l;
+                            int right_point_c = j + l;
 
-                            if(right_point_r >= n - 1 || right_point_c >= n || right_point_r + delta[0][0] * k >= n) break;
+                            if(right_point_r >= n - 1 || right_point_c >= n || right_point_r + k >= n) break;
 
-                            //유효한 꼭짓점 찾음
-                            List<Integer> number = new ArrayList<>();
+                            Set<Integer> number = new HashSet<>();
+                            int count = 0;
 
                             for(int m = 0; m <= l; m++){
-                                int num1 = dessert[i + delta[1][0] * m][j + delta[1][1] * m];
-                                int num2 = dessert[left_point_r + delta[1][0] * m][left_point_c + delta[1][1] * m];
+                                number.add(dessert[i + m][j + m]);
+                                number.add(dessert[left_point_r + m][left_point_c + m]);
 
-                                if(num1 == num2 || number.contains(num1) || number.contains(num2)) continue loop;
+                                count += 2;
 
-                                number.add(num1);
-                                number.add(num2);
+                                if(count != number.size()) continue loop;
                             }
 
                             for(int m = 1; m < k; m++){
-                                int num1 = dessert[i + delta[0][0] * m][j + delta[0][1] * m];
-                                int num2 = dessert[right_point_r + delta[0][0] * m][right_point_c + delta[0][1] * m];
+                                number.add(dessert[i + m][j - m]);
+                                number.add(dessert[right_point_r + m][right_point_c - m]);
 
-                                if(num1 == num2 || number.contains(num1) || number.contains(num2)) continue loop;
+                                count += 2;
 
-                                number.add(num1);
-                                number.add(num2);
+                                if(count != number.size()) continue loop;
                             }
 
                             max = max > number.size() ? max : number.size();
                         }
-
                     }
                 }
             }
