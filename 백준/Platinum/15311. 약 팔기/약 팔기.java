194d871eb9b6254a036d724845dbe6a6dd28c5d4
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-09-19 14:40:42 +0900

    [Platinum V] Title: 약 팔기, Time: 152 ms, Memory: 13548 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print("2000\n");
+
+        for(int i = 0; i < 1000; i++) System.out.print("1 ");
+
+        for(int i = 0; i < 1000; i++) System.out.print("1000 ");
+    }
+}
