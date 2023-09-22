04b83549a5e963e20f3aabd3b49bc0d4661465ff
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-09-22 10:39:29 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로2, Time: 128 ms, Memory: 26,408 KB -BaekjoonHub

---
+++ SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+import java.awt.Point;
+
+public class Solution {
+    public static boolean bfs(int[][] miro) {
+        Queue<Point> queue = new LinkedList<>();
+        boolean[][] visit = new boolean[100][100];
+        int[][] delta = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+        queue.offer(new Point(1, 1));
+        visit[1][1] = true;
+
+        while (!queue.isEmpty()) {
+            Point now = queue.poll();
+
+            for(int i = 0; i < 4; i++) {
+                int nr = now.x + delta[i][0];
+                int nc = now.y + delta[i][1];
+
+                if(nr < 0 || nr >= 100 || nc < 0 || nc >= 100 || miro[nr][nc] == 1 || visit[nr][nc]) continue;
+                else if(miro[nr][nc] == 3) return true;
+
+                queue.offer(new Point(nr, nc));
+                visit[nr][nc] = true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int t = 1; t <= 10; t++) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] miro = new int[100][100];
+
+            for(int i = 0; i < 100; i++) {
+                char[] chArr = br.readLine().toCharArray();
+
+                for(int j = 0; j < 100; j++) {
+                    miro[i][j] = chArr[j] - '0';
+                }
+            }
+
+            bw.write("#" + n);
+
+            if(bfs(miro)) bw.write(" 1\n");
+            else bw.write(" 0\n");
+        }
+
+        bw.close();
+    }
+}
