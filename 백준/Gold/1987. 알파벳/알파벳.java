1efbbf88da66f7140c55007c9884cc47e3479693
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-09-27 10:55:10 +0900

    [Gold IV] Title: 알파벳, Time: 2764 ms, Memory: 12540 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static boolean[] visit = new boolean[26];
+    public static char[][] alpha;
+    public static int[][] delta = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    public static int max = 0;
+
+    public static void dfs(int r, int c, int count) {
+
+        for(int i = 0; i < 4; i++) {
+            int nr = r + delta[i][0];
+            int nc = c + delta[i][1];
+
+            if(nr < 0 || nr >= alpha.length || nc < 0 || nc >= alpha[0].length || visit[alpha[r][c] - 'A']) continue;
+
+            visit[alpha[r][c] - 'A'] = true;
+            dfs(nr, nc, count + 1);
+            visit[alpha[r][c] - 'A'] = false;
+        }
+
+        max = Math.max(max, count);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        alpha = new char[r][c];
+
+        for(int i = 0; i < r; i++) {
+            alpha[i] = br.readLine().toCharArray();
+        }
+
+        dfs(0, 0, 0);
+
+        bw.write(max + "");
+        bw.close();
+    }
+}
