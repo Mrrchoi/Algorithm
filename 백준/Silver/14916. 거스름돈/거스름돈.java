c603c8a62cad601ec5a389d29891257f76c1025e
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-10-02 00:30:26 +0900

    [Silver V] Title: 거스름돈, Time: 76 ms, Memory: 11592 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int total = 0;
+
+        total += n / 5;
+        n %= 5;
+
+        if(total > 0 && n % 2 == 1) total += n / 2 + 2;
+        else if (n % 2 == 0) total += n / 2;
+        else total = -1;
+
+        bw.write(total + "");
+        bw.close();
+    }
+}
