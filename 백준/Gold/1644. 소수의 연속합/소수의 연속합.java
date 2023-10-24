12990701a90cb504086400c98109b4ee52884f52
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-10-24 11:28:38 +0900

    [Gold III] Title: 소수의 연속합, Time: 192 ms, Memory: 25256 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -2,22 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static boolean is_Prime(int n) {
-        for(int i = 2; i <= Math.sqrt(n); i++) {
-            if(n % i == 0) return false;
-        }
-
-        return true;
-    }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         ArrayList<Integer> list = new ArrayList<>();
         int n = Integer.parseInt(br.readLine());
+        boolean[] prime = new boolean[n + 1];
         int sum = 0, lidx = 0, ridx = 0, count = 0;
 
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(prime[i]) continue;
+            for(int j = i * i; j <= n; j += i) {
+                prime[j] = true;
+            }
+        }
+
         for(int i = 2; i <= n; i++) {
-            if(is_Prime(i)) list.add(i);
+            if(!prime[i]) list.add(i);
         }
 
         do {
