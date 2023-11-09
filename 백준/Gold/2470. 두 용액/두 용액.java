fe509669f233711ad06b5d2e9fa153f17fbb9d78
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-11-09 22:03:42 +0900

    [Gold V] Title: 두 용액, Time: 292 ms, Memory: 31580 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        int[] answer = new int[2];
+        int left = 0, right = n - 1, min = Integer.MAX_VALUE;
+
+        for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if(Math.abs(sum) < min) {
+                min = Math.abs(sum);
+                answer = new int[]{arr[left], arr[right]};
+            }
+
+            if (sum >= 0) {
+                right--;
+            }
+            else {
+                left++;
+            }
+        }
+
+        bw.write(answer[0] + " " + answer[1]);
+        bw.close();
+    }
+}
