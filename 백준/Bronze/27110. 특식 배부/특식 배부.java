8636aa0d0c6fa3d69dbabead4269610718cb307b
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-11-15 23:20:21 +0900

    [Bronze IV] Title: 특식 배부, Time: 80 ms, Memory: 11628 KB -BaekjoonHub

---
+++ 백준/Bronze/27110. 특식 배부/특식 배부.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        bw.write((Math.min(n, A) + Math.min(n, B) + Math.min(n, C)) + "");
+        bw.close();
+    }
+}
