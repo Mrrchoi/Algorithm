0535df956fb0f73d0d04f3413b290aaac13edc77
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-12-08 19:07:52 +0900

    [level 3] Title: 등굣길, Time: 0.43 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -10,14 +10,12 @@ class Solution {
         
         for(int i = 1; i < n + 1; i++) {
             for(int j = 1; j < m + 1; j++) {
-                if(dp[i][j] == -1) continue;
+                if(dp[i][j] == -1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
                 
-                int total = 0;
-                
-                if(dp[i - 1][j] != -1) total += dp[i - 1][j];
-                if(dp[i][j - 1] != -1) total += dp[i][j - 1];
-                
-                dp[i][j] = total % 1000000007;
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007;
             }
         }
         
