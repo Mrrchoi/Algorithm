97b2de9079c13468521571cbcc7e01cad7a708a9
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-12-08 19:04:24 +0900

    [level 3] Title: 등굣길, Time: 0.67 ms, Memory: 51.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] dp = new int[n + 1][m + 1];
+        
+        dp[0][1] = 1;
+        
+        for(int[] puddle : puddles) {
+            dp[puddle[1]][puddle[0]] = -1;
+        }
+        
+        for(int i = 1; i < n + 1; i++) {
+            for(int j = 1; j < m + 1; j++) {
+                if(dp[i][j] == -1) continue;
+                
+                int total = 0;
+                
+                if(dp[i - 1][j] != -1) total += dp[i - 1][j];
+                if(dp[i][j - 1] != -1) total += dp[i][j - 1];
+                
+                dp[i][j] = total % 1000000007;
+            }
+        }
+        
+        // for(int[] arr : dp) {
+        //     System.out.println(Arrays.toString(arr));
+        // }
+        
+        return dp[n][m];
+    }
+}
