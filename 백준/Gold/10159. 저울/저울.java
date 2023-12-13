ad73f903b7bf6b9d92fbe82d9cf209b4567ca616
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-12-13 17:22:31 +0900

    [Gold IV] Title: 저울, Time: 112 ms, Memory: 12604 KB -BaekjoonHub

---
+++ 백준/Gold/10159. 저울/저울.java
@@ -0,0 +1,54 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int dfs_normal(boolean[][] graph, boolean[] visit, int n, int u) {
+        int count = 0;
+
+        visit[u] = true;
+
+        for(int v = 1;  v < n + 1; v++) {
+            if(graph[v][u] && !visit[v]) {
+                count++;
+                count += dfs_normal(graph, visit, n, v);
+            }
+        }
+
+        return count;
+    }
+    public static int dfs_reverse(boolean[][] graph, boolean[] visit, int n, int u) {
+        int count = 0;
+
+        visit[u] = true;
+
+        for(int v = 1;  v < n + 1; v++) {
+            if(graph[u][v] && !visit[v]) {
+                count++;
+                count += dfs_reverse(graph, visit, n, v);
+            }
+        }
+
+        return count;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        boolean[][] graph = new boolean[n + 1][n + 1];
+
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a][b] = true;
+        }
+
+        for(int i = 1; i < n + 1; i++) {
+            bw.write((n - (dfs_normal(graph, new boolean[n + 1], n, i) + dfs_reverse(graph, new boolean[n + 1], n, i) + 1)) + "\n");
+        }
+
+        bw.close();
+    }
+}
