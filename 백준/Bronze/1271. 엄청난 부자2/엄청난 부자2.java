535ff2a37718a00ed716655bd93a6c6a110d970c
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2023-12-24 17:44:38 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 88 ms, Memory: 11708 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger n = new BigInteger(st.nextToken());
+        BigInteger m = new BigInteger(st.nextToken());
+
+        bw.write(n.divide(m) + "\n" + n.remainder(m));
+        bw.close();
+    }
+}
