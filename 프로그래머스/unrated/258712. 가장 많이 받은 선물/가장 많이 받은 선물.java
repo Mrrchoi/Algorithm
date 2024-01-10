5522ed7da337c347d7f9c5b89d2880958088612c
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2024-01-10 10:18:26 +0900

    [level unrated] Title: 가장 많이 받은 선물, Time: 4.47 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        Map<String, Integer>[] giveNtake = new HashMap[friends.length];
+        Map<String, Integer>[] log = new HashMap[friends.length];
         Map<String, Integer> friendsMap = new HashMap<>();
         int[] figure = new int[friends.length];
         int[] result = new int[friends.length];
@@ -10,7 +10,7 @@ class Solution {
         
         for(int i = 0; i < friends.length; i++) {
             friendsMap.put(friends[i], i);
-            giveNtake[i] = new HashMap<>();
+            log[i] = new HashMap<>();
         }
         
         for(String gift : gifts) {
@@ -18,7 +18,7 @@ class Solution {
             int idx1 = friendsMap.get(str[0]);
             int idx2 = friendsMap.get(str[1]);
             
-            giveNtake[idx1].put(str[1], giveNtake[idx1].getOrDefault(str[1], 0) + 1);
+            log[idx1].put(str[1], log[idx1].getOrDefault(str[1], 0) + 1);
             
             figure[idx1]++;
             figure[idx2]--;
@@ -26,8 +26,8 @@ class Solution {
         
         for(int i = 0; i < friends.length - 1; i++) {
             for(int j = i + 1; j < friends.length; j++) {
-                int num1 = giveNtake[i].getOrDefault(friends[j], 0);
-                int num2 = giveNtake[j].getOrDefault(friends[i], 0);
+                int num1 = log[i].getOrDefault(friends[j], 0);
+                int num2 = log[j].getOrDefault(friends[i], 0);
                 
                 if(num1 > num2 || (num1 == num2 && figure[i] > figure[j])) {
                     result[i]++;
