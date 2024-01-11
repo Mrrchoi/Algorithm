c95a38eb18b45a49a0d85e8b007b51d4a053f484
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2024-01-11 09:13:45 +0900

    [Silver III] Title: 블로그, Time: 288 ms, Memory: 38424 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 1; i < n + 1; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        long sum = 0, max= 0;
+        int count = 0;
+
+        for(int i = 1; i < x; i++) {
+            sum += arr[i];
+        }
+
+        for(int i = x; i < n + 1; i++) {
+            sum += arr[i] - arr[i - x];
+
+            if(max < sum) {
+                max = Math.max(max, sum);
+                count = 1;
+            }
+            else if(max == sum) {
+                count++;
+            }
+        }
+
+        bw.write((max == 0 ? "SAD" : max + "\n" + count));
+        bw.close();
+    }
+}
