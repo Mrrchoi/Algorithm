8785a8ddfd95c21e28d545abb90a5d824c14653b
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2024-04-03 23:18:20 +0900

    [Bronze V] Title: CAPS, Time: 216 ms, Memory: 24500 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s = br.readLine();
+        
+        bw.write(s.toUpperCase());
+        bw.close();
+    }
+}
