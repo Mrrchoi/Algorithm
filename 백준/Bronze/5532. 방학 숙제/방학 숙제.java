283e4a4d5993d7ecd9b19fa3a9d5f8fe9382d976
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2024-04-18 23:46:02 +0900

    [Bronze IV] Title: 방학 숙제, Time: 80 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int l = Integer.parseInt(br.readLine());
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+
+        bw.write((l - Math.max(a / c + (a % c == 0 ? 0 : 1), b / d + (b % d == 0 ? 0 : 1))) + "");
+        bw.close();
+    }
+}
