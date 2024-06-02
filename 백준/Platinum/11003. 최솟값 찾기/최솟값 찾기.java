f8cb390e984139de742826a0b254a030576db23c
Mrrchoi <139312405+Mrrchoi@users.noreply.github.com>
2024-06-02 19:52:26 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2520 ms, Memory: 700052 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            while (!deque.isEmpty() && i - deque.peekFirst() >= l) {
+                deque.pollFirst();
+            }
+
+            if (deque.isEmpty() || arr[i] <= arr[deque.peekFirst()]) {
+                deque.pollFirst();
+                deque.addFirst(i);
+            }
+            else {
+                while (i - deque.peekLast() >= l || arr[i] <= arr[deque.peekLast()]) {
+                    deque.pollLast();
+                }
+
+                deque.addLast(i);
+            }
+
+            bw.write(arr[deque.peekFirst()] + " ");
+        }
+
+        bw.close();
+    }
+}
